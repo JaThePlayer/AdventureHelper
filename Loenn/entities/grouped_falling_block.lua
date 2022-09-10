f6f96bdcd17a9ef8bb3cd73d7cd41bf540818a82
catapillie <espritox@gmail.com>
2022-09-10 14:24:06 +0200

    Add Grouped Falling Block plugin

---
+++ Loenn/entities/grouped_falling_block.lua
@@ -0,0 +1,20 @@
+local fakeTilesHelper = require "helpers.fake_tiles"
+
+local groupedFallingBlock = {}
+
+groupedFallingBlock.name = "AdventureHelper/GroupedFallingBlock"
+
+groupedFallingBlock.placements = {
+    name = "grouped_falling_block",
+    data = {
+        width = 8,
+        height = 8,
+        tiletype = "3",
+        climbFall = true
+    }
+}
+
+groupedFallingBlock.sprite = fakeTilesHelper.getEntitySpriteFunction("tiletype", false)
+groupedFallingBlock.fieldInformation = fakeTilesHelper.getFieldInformation("tiletype")
+
+return groupedFallingBlock
