bc6ead476916520171d6d7ccddbed4c9c071771d
JaThePlayer <janmalenta@gmail.com>
2024-07-08 13:20:00 +0200

    Implement camera culling for zip movers

---
+++ Code/Utils.cs
@@ -0,0 +1,28 @@
+﻿using Celeste.Mod.Helpers;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.AdventureHelper {
+    internal static class Utils {
+        public static bool IsLineVisible(Vector2 start, Vector2 end) {
+            float left, right;
+            if (start.X > end.X) {
+                left = end.X;
+                right = start.X;
+            } else {
+                left = start.X;
+                right = end.X;
+            }
+
+            float top, bot;
+            if (start.Y > end.Y) {
+                top = end.Y;
+                bot = start.Y;
+            } else {
+                top = start.Y;
+                bot = end.Y;
+            }
+
+            return CullHelper.IsRectangleVisible(left, top, right - left, bot - top);
+        }
+    }
+}
